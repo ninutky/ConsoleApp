3ff92017ddfa8b7bac08a4437c053306a5f07374
Nagyeong Park <zzum0701@naver.com>
2023-04-20 14:05:29 +0900

    초기 커밋.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if(x % 5 == 0)
+                {
+                    Console.WriteLine("__@");
+                } else if (x % 5 == 1)
+                {
+                    Console.WriteLine("_^@");
+                } else
+                {
+                    Console.WriteLine("^_@");
+                }
+                Thread.Sleep(1000);
+                x++;
+            }
+        }
+    }
+}
